7c7fe432de20b58e5b2a86f4d02973959427ad29
Hyegyeong310 <hyegyeong310@gmail.com>
2019-09-09 03:14:05 +0900

    [Add] call topRated, popular, airingToday api in TVContainer

--- src/Route/TV/TVContainer.js
+++ src/Route/TV/TVContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import TVPresenter from './TVPresenter';
+import { tvApi } from 'api';
 
 export default class extends Component {
   state = {
@@ -9,6 +10,31 @@ export default class extends Component {
     error: null,
     loading: true
   };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: topRated }
+      } = await tvApi.topRated();
+      const {
+        data: { results: popular }
+      } = await tvApi.popular();
+      const {
+        data: { results: airingToday }
+      } = await tvApi.airingToday();
+
+      this.setState({
+        topRated,
+        popular,
+        airingToday
+      });
+    } catch (error) {
+      this.setState({ error: `Can't find TV information: ${error}` });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
   render() {
     const { topRated, popular, airingToday, error, loading } = this.state;
     return (

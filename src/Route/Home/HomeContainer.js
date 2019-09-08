53bc9daa4ab239957c0143478e1b69f108ef727e
Hyegyeong310 <hyegyeong310@gmail.com>
2019-09-09 03:13:03 +0900

    [Add] call nowPlaying, upcoming, popular api in HomeContainer

--- src/Route/Home/HomeContainer.js
+++ src/Route/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import HomePresenter from './HomePresenter';
+import { moviesApi } from 'api';
 
 export default class extends Component {
   state = {
@@ -9,6 +10,34 @@ export default class extends Component {
     error: null,
     loading: true
   };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying }
+      } = await moviesApi.nowPlaying();
+      const {
+        data: { results: upcoming }
+      } = await moviesApi.upcoming();
+      const {
+        data: { results: popular }
+      } = await moviesApi.popular();
+      this.setState({
+        nowPlaying,
+        upcoming,
+        popular
+      });
+    } catch (error) {
+      this.setState({
+        error: `Can't find movie information: ${error}`
+      });
+    } finally {
+      this.setState({
+        loading: false
+      });
+    }
+  }
+
   render() {
     const { nowPlaying, upcoming, popular, error, loading } = this.state;
     return (

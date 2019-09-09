2030572fa632795604d4eee26d947ba2b6ac3723
Hyegyeong310 <hyegyeong310@gmail.com>
2019-09-09 18:26:59 +0900

    [Add] Section Component, applyed to HomePresenter

--- src/Route/Home/HomePresenter.js
+++ src/Route/Home/HomePresenter.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Section from 'Components/Section';
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
 
 const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
-  null;
+  loading ? null : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map(movie => movie.original_title)}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Movies">
+          {upcoming.map(movie => movie.original_title)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Movies">
+          {popular.map(movie => movie.original_title)}
+        </Section>
+      )}
+    </Container>
+  );
 
 HomePresenter.proptyles = {
   nowPlaying: PropTypes.array,

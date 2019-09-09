5b99074dc8f9eb368e8426333a159942c1fa1f43
Hyegyeong310 <hyegyeong310@gmail.com>
2019-09-09 22:54:39 +0900

    [Add] show movies, tv shows title

--- src/Route/Home/HomePresenter.js
+++ src/Route/Home/HomePresenter.js
@@ -2,27 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Section from 'Components/Section';
+import Loader from 'Components/Loader';
 
 const Container = styled.div`
-  padding: 0px 10px;
+  padding: 0px 20px;
 `;
 
 const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
-  loading ? null : (
+  loading ? (
+    <Loader />
+  ) : (
     <Container>
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
-          {nowPlaying.map(movie => movie.original_title)}
+          {nowPlaying.map(movie => (
+            <span key={movie.id}>{movie.original_title}</span>
+          ))}
         </Section>
       )}
       {upcoming && upcoming.length > 0 && (
         <Section title="Upcoming Movies">
-          {upcoming.map(movie => movie.original_title)}
+          {upcoming.map(movie => (
+            <span key={movie.id}>{movie.original_title}</span>
+          ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Movies">
-          {popular.map(movie => movie.original_title)}
+          {popular.map(movie => (
+            <span key={movie.id}>{movie.original_title}</span>
+          ))}
         </Section>
       )}
     </Container>

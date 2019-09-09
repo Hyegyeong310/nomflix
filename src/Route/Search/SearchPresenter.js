4f6ff486ea97162dd9a7e6c2a26bd09fa590c394
Hyegyeong310 <hyegyeong310@gmail.com>
2019-09-09 23:23:57 +0900

    [Add] Error message component

--- src/Route/Search/SearchPresenter.js
+++ src/Route/Search/SearchPresenter.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Loader from 'Components/Loader';
 import Section from 'Components/Section';
+import Message from 'Components/Message';
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -54,6 +55,13 @@ const SearchPresenter = ({
             ))}
           </Section>
         )}
+        {error && <Message color="#e74c3c" text={error} />}
+        {showResults &&
+          movieResults &&
+          showResults.length === 0 &&
+          movieResults.length === 0 && (
+            <Message color="#95a5a6" text="Nothing found"></Message>
+          )}
       </>
     )}
   </Container>
